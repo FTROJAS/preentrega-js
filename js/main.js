99a20ac42e47905315ae807430120402942593bb
FTROJAS <facundo_rojas2424@hotmail.com>
2024-04-10 22:08:58 -0300

    segunda entrega

--- js/main.js
+++ js/main.js
@@ -1,48 +1,59 @@
 const nombre = prompt ("ingrese su nombre")
-const apellido = prompt ("ingrese su apellido")
-const edad = parseInt(prompt("Hola, " + nombre + " Ingrese la edad"));
+const apellido =prompt ("ingrese su apellido")
 
 
 
-if (edad <18) {
-        console.log("edad no permitida")
-        alert("Bienvenido, " + nombre + ".La edad permitida es a partir de 18 (diesiocho) años");
-    }
+const edad=(prompt("Hola, " + nombre + " Ingrese la edad"));
 
-if (edad >18) {
+function datos(saludo){
+    console.log(saludo)
+}
+
+datos("Bienvenido, Los datos del usuario son:")
+
+console.log(nombre+ " "+ apellido+ ", "+ edad +" de edad.")
+
+
+while (edad<18 && edad<30 && edad<45 && edad<65) {
+    alert("Bienvenido, " + nombre + " .La edad permitida es a partir de 18 (diesiocho) años");
+    console.log("edad no permitida")
+    console.log("fin del programa")
+    alert("fin del programa")
+    
+}
+
+
+
+
+if(edad<30) {
     console.log("la cobertura para " + nombre + " es de $30.000" )
 
     alert("Bienvenido, " + nombre + " analizando su edad le ofrecemos la mejor cobertura, con un monto de $30.000");
-}
-if (edad < 30) {
-    console.log ("la cobertura para " + nombre + " es de $30.000" )
-    alert("Bienvenido, " + nombre + " analizando su edad le ofrecemos la mejor cobertura, con un monto de $30.000");
+    console.log ("fin del programa")
+    alert ("fin del programa")
 }
 
 
-if (edad  > 30) {
-    console.log("la cobertura para " + nombre + " es de $65.000" );
+else if(edad<45) {
+    console.log("la cobertura para " + nombre + "es de $65.000" );
     alert("Bienvenido, " + nombre + " analizando su edad le ofrecemos la mejor cobertura, con un monto de $65.000");
-}
-if (edad < 45) {
-    console.log("la cobertura para " + nombre + " es de $65.000" );
-    alert("Bienvenido, " + nombre + " analizando su edad le ofrecemos la mejor cobertura, con un monto de $65.000");
+    console.log ("fin del programa")
+    alert ("fin del programa")
 }
 
 
-if (edad > 45) {
-    console.log("la cobertura para " + nombre + " es de $100.000" );
+else if(edad<65) {
+    console.log("la cobertura para " + nombre + "es de $100.000" );
     alert("Bienvenido, " + nombre + " analizando su edad le ofrecemos la mejor cobertura, con un monto de $100.000");
+    console.log ("fin del programa")
+    alert ("fin del programa")
 }
 
-if (edad <65) {
-    console.log("la cobertura para " + nombre + " es de $100.000" );
-    alert("Bienvenido, " + nombre + " analizando su edad le ofrecemos la mejor cobertura, con un monto de $100.000");
-}
-
-if (edad >65 ) {
-    console.log("la cobertura para " + nombre + " es de $150.000" );
+else {
+    console.log("la cobertura para " + nombre + "es de $150.000" );
     alert("Bienvenido, " + nombre + " analizando su edad le ofrecemos la mejor cobertura, con un monto de $150.000");
-}
-
+    console.log ("fin del programa")
+    alert ("fin del programa")
+    }
+    
 
